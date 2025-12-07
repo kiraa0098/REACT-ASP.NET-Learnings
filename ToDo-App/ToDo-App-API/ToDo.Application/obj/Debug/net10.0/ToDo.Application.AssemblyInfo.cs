cd80f4f84915ec226ed1c284c1d1f2cc27fbcc36
kiraa0098 <alwynnqq0098@gmail.com>
2025-12-07 10:34:54 +0800

    s

--- ToDo-App/ToDo-App-API/ToDo.Application/obj/Debug/net10.0/ToDo.Application.AssemblyInfo.cs
+++ ToDo-App/ToDo-App-API/ToDo.Application/obj/Debug/net10.0/ToDo.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cdc8daa214ab40b1484778afbe905dd755cb5c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
